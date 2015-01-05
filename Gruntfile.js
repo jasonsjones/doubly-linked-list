ed3b7defa5fcc7c04e03212391f58e6f513eebd4
Jason S. Jones <jsjones96@gmail.com>
2015-01-05 06:32:46 -0800

    add grunt task runner to project for linting

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            all: {
+                src: ['doubly-linked-list.js', 'tests/**/*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
+};
