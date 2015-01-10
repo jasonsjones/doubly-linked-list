6a05943df515d747e8cf41e5d6b1f20e3594b17c
Jason S. Jones <jsjones96@gmail.com>
2015-01-10 07:20:57 -0800

    add mocha tests to default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,18 @@ module.exports = function(grunt) {
             options: {
                 jshintrc: '.jshintrc'
             }
+        },
+        mochacli: {
+            options: {
+                reporter: 'spec',
+                bail: true
+            },
+            all: ['test/*.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-cli');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'mochacli']);
 };
